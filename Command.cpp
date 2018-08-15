08f85b527fa6ea95619e1a7dc7e934ee3ce3cc47
Vitalii <vt_ostap@ukr.net>
2018-08-15 11:39:01 +0300

    parse command

--- Command.cpp
+++ Command.cpp
@@ -12,27 +12,63 @@ Command::~Command()
 {
 }
 
-int CommandList::call(const std::string& param) const
+int CommandList::call() const
 {
+	switch (m_action)
 	{
-		switch (action)
-		{
-		case read_file:
-			m_list.ReadFromFile(param);
-			break;
-		case new_name:
-			m_list.AddNewName(param);
-			break;
-		case print_list:
-			break;
-		case compute_score:
-			std::cout << m_list.ComputeList();
-			break;
-		case quit:
-			return -1;
-			break;
+	case read_file:
+		m_list.ReadFromFile(m_param);
+		break;
+	case new_name:
+		m_list.AddNewName(m_param);
+		break;
+	case print_list:
+		break;
+	case compute_score:
+		std::cout << m_list.ComputeList();
+		break;
+	case quit:
+		return -1;
+		break;
 
-		default: break;
-		}
+	default: 
+		break;
 	}
+	return 1;
+}
+
+std::pair<std::string, std::string> CommandList::ParseCommand(const std::string str)
+{
+	std::pair<std::string, std::string> result;
+	size_t pos = str.find(':');
+	if (pos == std::string::npos)
+	{
+		std::cout << "Wrong command" << std::endl;
+		return result;
+	}
+	std::string command = str.substr(pos);
+
+	bool first = false;
+	bool second = false;
+	for (auto iter = str.begin(); iter != str.end(); ++iter)
+	{
+		if (*iter != ' ' && second == false)
+			first = true;
+		if (*iter == ' ' && first && second == false)
+		{
+			second = true;
+			first = false;
+		}
+
+		if (*iter != ' ' && first)
+			result.first.push_back(*iter);
+		if (*iter != ' ' && second)
+			result.second.push_back(*iter);
+	}
+	return result;
+}
+
+CommandList::CommandList(ListWords& list, const std::string& str)
+	:m_list(list)
+{
 }
--- Command.h
+++ Command.h
@@ -7,13 +7,17 @@ public:
 	Command();
 	~Command();
 
-	virtual int call(const std::string& param = "") const = 0;
+	virtual int call() const = 0;
 };
 
 class CommandList : public Command
 {
 	ListWords& m_list;
-	enum Action { read_file, new_name, print_list, compute_score, quit} action;
-
-	int call(const std::string& param = "") const override;
+	enum Action { read_file, new_name, print_list, compute_score, quit} m_action;
+	std::string m_param;
+public:
+	CommandList(ListWords& list, const std::string& str);
+	int call() const override;
+private:
+	std::pair<std::string, std::string> ParseCommand(const std::string str);
 };
